5189a393cc07874ae1b1dc10c0a1cf3ad299dfa6
choiyc05 <choiyc05@naver.com>
2026-01-05 18:23:14 +0900

    260105_1823 upload

--- 260105/Study03/src/Select.jsx
+++ 260105/Study03/src/Select.jsx
@@ -1,6 +1,56 @@
+import { useNavigate, useLocation } from 'react-router'
+
 const Select = () => {
-    <> Select
-    </>
+    const location = useLocation();
+    const userKey = location.state.userKey;
+
+    const userdata = [{ key : 1, name : "스티븐", email : "jobs@shellfolder.com", pwd : "111", gender : true },
+               		 { key : 2, name : "에브릴", email : "lavigne@shellfolder.com", pwd : "222", gender : false }]
+
+    return (
+    <div className="container mt-3">
+	  <h1 className="display-1 text-center">사용자 정보</h1>
+		<form>
+		  <div className="mb-3 mt-3">
+		    <label htmlFor="name" className="form-label">이름:</label>
+		    <input type="text" className="form-control" id="name" placeholder="이름을 입력하세요." name="name" readOnly="readonly" />
+		  </div>
+		  <div className="mb-3 mt-3">
+		    <label htmlFor="email" className="form-label">이메일:</label>
+		    <input type="email" className="form-control" id="email" placeholder="이메일를 입력하세요." name="email" readOnly="readonly" />
+		  </div>
+		  <div className="mb-3">
+		    <label htmlFor="pwd" className="form-label">비밀번호:</label>
+		    <input type="password" className="form-control" id="pwd" placeholder="비밀번호를 입력하세요." name="pwd" readOnly="readonly" />
+		  </div>
+			<div className="d-flex">
+			  <div className="p-2 flex-fill">
+			  	<div className="form-check">
+					<input type="radio" className="form-check-input" id="radio1" name="optradio" value="1" onChange={() => {}} />남성
+					<label className="form-check-label" htmlFor="radio1"></label>
+				</div>
+			  </div>
+			  <div className="p-2 flex-fill">
+			  	<div className="form-check">
+					<input type="radio" className="form-check-input" id="radio2" name="optradio" value="2" onChange={() => {}} />여성
+					<label className="form-check-label" htmlFor="radio2"></label>
+				</div>
+			  </div>
+			</div>
+		</form>
+		<div className="d-flex">
+		  <div className="p-2 flex-fill d-grid">
+		  	<a href="Update" className="btn btn-primary">수정</a>
+		  </div>
+		  <div className="p-2 flex-fill d-grid">
+				<a href="./" className="btn btn-primary">삭제</a>
+		  </div>
+		  <div className="p-2 flex-fill d-grid">
+			<a href="./" className="btn btn-primary">취소</a>
+		  </div>
+		</div>
+	</div>
+    )
 }
 
 export default Select
